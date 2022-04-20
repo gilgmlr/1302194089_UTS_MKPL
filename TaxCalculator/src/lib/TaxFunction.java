ab2867347964eeff2ce93775ac42bc5f4adee324
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-04-20 21:59:54 +0700

    Memisahkan method calculateTax
    Membuat beberapa method baru

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -19,26 +19,35 @@ public class TaxFunction {
 		
 		int tax = 0;
 		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
+		printNumberOfMontWorking(numberOfMonthWorking);
 		
+		childrenNumbersMax(numberOfChildren);
+		
+		tax = isMarried(isMarried, monthlySalary, otherMonthlyIncome, numberOfMonthWorking, deductible, numberOfChildren);
+		
+		return tax<0? 0 : tax;
+	}
+
+	public static void childrenNumbersMax(int numberOfChildren){
 		if (numberOfChildren > 3) {
 			numberOfChildren = 3;
 		}
-		
+	}
+
+	public static void printNumberOfMontWorking(int numberOfMonthWorking){
+		if (numberOfMonthWorking > 12) {
+			System.err.println("More than 12 month working per year");
+		}
+	}
+
+	public static int isMarried(boolean isMarried, int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, int numberOfChildren){
+		int tax;
 		if (isMarried) {
 			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
 		}else {
 			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
 		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
+		return tax;
 	}
 	
 }
