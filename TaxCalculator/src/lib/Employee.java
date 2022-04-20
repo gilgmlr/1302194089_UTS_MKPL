7f4575655aefd630713dc3c8d4d438e387491d58
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-04-20 14:32:39 +0700

    Menggabungkan & mengubah tipe data tanggal Joined
    Menghapus atribut
      private int yearJoined;
      private int monthJoined;
      private int dayJoined;
    
    dan menambahkan atribut
      private LocalDate joinDate;
    
    menyesuaikan Constructor Employee

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -18,9 +18,7 @@ public class Employee {
 	private String idNumber;
 	private String address;
 	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
+	private LocalDate joinDate;
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
@@ -36,15 +34,13 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, Gender gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, LocalDate joinDate, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
+		this.joinDate = joinDate;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
 		
